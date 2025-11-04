662704c358d9603ca4fc5bd1082004cdcbebf523
adev05 <andrew.nasibulin@mail.ru>
2025-11-04 21:46:58 +0300

    first commit

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	HOST: 'postgresql.57ea27508d60.hosting.myjino.ru',
+	USER: 'j70168117_azarov7777',
+	PORT: 5432,
+	PASSWORD: 'r4nRuY25nyCZSdqd3GMX',
+	DB: 'j70168117_azarov7777',
+	dialect: 'postgres',
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+}
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
